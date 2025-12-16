57394d23876df906b14a055b12c3bd8333e0ec70
JosueMontoyaDuron <202402358@umh.edu.hn>
2025-12-16 14:15:41 -0600

    Add files via upload

---
+++ Controllers/CuentaController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Cuentas.Data;
+using Cuentas.Models;
+
+namespace Cuentas.Controllers
+{
+    public class CuentaController : Controller
+    {
+        private readonly CuentasContext _context;
+
+        public CuentaController(CuentasContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Cuenta
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Cuenta.ToListAsync());
+        }
+
+        // GET: Cuenta/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cuenta = await _context.Cuenta
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cuenta == null)
+            {
+                return NotFound();
+            }
+
+            return View(cuenta);
+        }
+
+        // GET: Cuenta/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cuenta/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,numero,descripcion,creditos,debitos,balance")] Cuenta cuenta)
+        {
+            if (ModelState.IsValid)
+            {
+                cuenta.balance = cuenta.creditos - cuenta.debitos;
+
+                _context.Add(cuenta);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cuenta);
+        }
+
+        // GET: Cuenta/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cuenta = await _context.Cuenta.FindAsync(id);
+            if (cuenta == null)
+            {
+                return NotFound();
+            }
+            return View(cuenta);
+        }
+
+        // POST: Cuenta/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var cuentaToUpdate = await _context.Cuenta.FindAsync(id);
+
+            if (cuentaToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            // Se permite actualizar: descripcion, creditos y debitos. El balance se calcula.
+            if (await TryUpdateModelAsync<Cuenta>(
+                cuentaToUpdate,
+                "",
+                c => c.descripcion, c => c.creditos, c => c.debitos))
+            {
+                cuentaToUpdate.balance = cuentaToUpdate.creditos - cuentaToUpdate.debitos;
+
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+                        await _context.SaveChangesAsync();
+                    }
+                    catch (DbUpdateConcurrencyException)
+                    {
+                        if (!CuentaExists(cuentaToUpdate.Id))
+                        {
+                            return NotFound();
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            return View(cuentaToUpdate);
+        }
+
+        // GET: Cuenta/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cuenta = await _context.Cuenta
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cuenta == null)
+            {
+                return NotFound();
+            }
+
+            return View(cuenta);
+        }
+
+        // POST: Cuenta/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cuenta = await _context.Cuenta.FindAsync(id);
+            if (cuenta != null)
+            {
+                _context.Cuenta.Remove(cuenta);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CuentaExists(int id)
+        {
+            return _context.Cuenta.Any(e => e.Id == id);
+        }
+    }
+}
